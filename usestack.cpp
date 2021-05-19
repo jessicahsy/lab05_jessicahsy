49ea082e3df7501ab1d072b19aef5dc3af50ea6e
Jessica Ho <shin-yi@ucsb.edu>
2021-05-19 15:27:28 -0700

    change smth

--- usestack.cpp
+++ usestack.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int main() {
 //____________use_stack____________
+    cout<<"\n_____use_stack_____\n";
     Stack s;
     s.push(10);
     s.push(20);
@@ -23,17 +24,17 @@ int main() {
     cout<<"expect 0. size: "<<s.size()<<endl;
     s.pop();
     cout<<"s next: "<<s.get_next()<<endl;
-    s.push(9);
-    s.push(8);
-    s.push(7);
-    s.push(6);
-    s.push(5);
-    s.push(4);
-    s.push(3);
-    s.push(2);
-    s.push(1);
-    s.push(0);
+    s.push(99);
+    s.push(88);
+    s.push(77);
+    s.push(66);
+    s.push(55);
+    s.push(44);
+    s.push(33);
+    s.push(22);
     s.push(11);
+    s.push(0);
+    s.push(1);
     cout<<"expect 10. size: "<<s.size()<<endl;
     cout<<"s full? "<<s.full()<<endl;
 
@@ -45,7 +46,43 @@ int main() {
 
 
 //____________postfix______________
-// evaluate "5 2 - 3 *"
+cout<<"_____postfix_____(i)\n";
+//evaluating "5 2 - 3 *"
+
+//start with an empty stack
+
+Stack numbers;
+
+// first two tokens- push "5 2":
+numbers.push(5);
+numbers.push(2);
+
+// third token is calculation to do "-":
+int right = numbers.top();
+numbers.pop();
+int left = numbers.top();
+numbers.pop();
+numbers.push(left - right);
+//top is 3
+
+// fourth token- push "3":
+numbers.push(3);
+
+// fifth is another calculation "*":
+right = numbers.top();
+numbers.pop();
+left = numbers.top();
+numbers.pop();
+numbers.push(left * right);
+//top is 9
+
+// done - print result:
+cout << numbers.top() << endl;
+
+
+// i'm still confused about this postfix section
+// including this just in case...
+cout<<"_____postfix_____(ii)\n";
     Stack post;
     cout<<"Enter postfix>>  ";
     char input[30], *tokens[15];
@@ -64,7 +101,7 @@ int main() {
         if ((tok>='0')&&(tok<='9')){
             int T=tok-'0';                  //convert tok to int var so it can be
             post.push(T);                   //pushed and/or used w/ arithmetic operations
-        }else{
+        }else{                              //only works with int 0-9...
             if (post.get_next()<2){cout<<"bad expression: unexpected operator"<<endl;return 1;}
             else{
                 right = post.top();
@@ -82,41 +119,6 @@ int main() {
     }
     if (post.get_next()!=1){cout<<"bad expression: operator expected"<<endl;return 1;}
     cout<<"Result: "<<post.top()<<endl;
-
-// i'm still confused about this postfix section
-// including this just in case...
-
-// evaluating "5 2 - 3 *"
-
-// start with an empty stack
-
-// Stack numbers;
-
-// // first two tokens- push "5 2":
-// numbers.push(5);
-// numbers.push(2);
-
-// // third token is calculation to do "-":
-// int right = numbers.top();
-// numbers.pop();
-// int left = numbers.top();
-// numbers.pop();
-// numbers.push(left - right);
-// //top is 3
-
-// // fourth token- push "3":
-// numbers.push(3);
-
-// // fifth is another calculation "*":
-// right = numbers.top();
-// numbers.pop();
-// left = numbers.top();
-// numbers.pop();
-// numbers.push(left * right);
-// //top is 9
-
-// // done - print result:
-// cout << numbers.top() << endl;
 
 
     return 0;
